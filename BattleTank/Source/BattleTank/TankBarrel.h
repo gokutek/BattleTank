6155aadfc3b199ab35e01e0d17c727b75791d249
gokutek <vxer@qq.com>
2019-04-22 22:07:00 +0800

    BlueprintSpawnableComponent()

--- BattleTank/Source/BattleTank/TankBarrel.h
+++ BattleTank/Source/BattleTank/TankBarrel.h
@@ -3,17 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/InstancedStaticMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "TankBarrel.generated.h"
 
 /**
  * 
  */
-UCLASS()
-class BATTLETANK_API UTankBarrel : public UInstancedStaticMeshComponent
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), hideCategories = ("Collision"))
+class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    float MaxDegreesPerSeconds = 20;
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    float MaxElevateDegrees = 40; // 炮筒向上可以抬起的最大角度
+    
+    UPROPERTY(EditAnywhere, Category = Setup)
+    float MinDegreesPerSeconds = 0; // 炮筒向下可以抬起的最大角度
+
 public:
     void Elevate();
+
+
 };
