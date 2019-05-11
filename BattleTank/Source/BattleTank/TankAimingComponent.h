bb871bb2856bc0b724bef2e954b3123e8e4c69ff
gokutek <vxer@qq.com>
2019-05-11 21:18:01 +0800

    补充注释

--- BattleTank/Source/BattleTank/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/TankAimingComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -21,8 +21,10 @@ public:
     // Called every frame
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+    // 将炮筒以指定的速度瞄向指定的点
     void AimAt(FVector const &HitLocation, float LaunchSpeed);
 
+    // 设置炮筒组件的引用
     void SetBarrel(UTankBarrel* Barrel);
 
 protected:
