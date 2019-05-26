93609c1373337024f807b51cc981aa03897dcb08
gokutek <vxer@qq.com>
2019-05-26 15:01:04 +0800

    Setup Track Throttles

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -2,6 +2,7 @@
 
 #include "TankTrack.h"
 
-
-
-
+void UTankTrack::SetThrottle(float Throttle)
+{
+    UE_LOG(LogTemp, Warning, TEXT("SetThrottle: %s %f"), *GetName(), Throttle);
+}
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -7,14 +7,15 @@
 #include "TankTrack.generated.h"
 
 /**
- * 
+ * UTankTrack is used to set maximum driving force, and to apply forces to tank.
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+public:
+    // Sets a throttle between -1 and +1
+    UFUNCTION(BlueprintCallable, Category = Input)
+    void SetThrottle(float Throttle);
 };
