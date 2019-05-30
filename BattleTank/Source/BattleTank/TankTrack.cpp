8b19a4fdf876f09fa70000d0d739bd9e4614579b
gokutek <vxer@qq.com>
2019-05-30 22:40:19 +0800

    Physics Materials & Friction

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -4,6 +4,7 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
+    // 给坦克的根组件施加一个力
     FVector Force = GetForwardVector() * Throttle * TrackMaxDrivingForce;
     UPrimitiveComponent* Tank = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
     Tank->AddForceAtLocation(Force, GetComponentLocation());
